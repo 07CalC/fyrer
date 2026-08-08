0b8ffde12b0379c1e85c85dd8d82b5fd8d188f63
07CalC <maheshwarivinayak90@gmail.com>
2026-08-08 12:04:38 +0530

    fix: atomic cache save

--- src/cache/local.rs
+++ src/cache/local.rs
@@ -11,7 +11,23 @@ use crate::{
 pub struct LocalCacheProvider;
 
 impl CacheProvider for LocalCacheProvider {
-    fn restore(&self, _: &str) -> anyhow::Result<bool> {
+    fn restore(&self, key: &str) -> anyhow::Result<bool> {
+        let fyrer_dir = Path::new(DEFAULT_FYRER_DIR);
+        let cache_dir = fyrer_dir.join("cache").join(key);
+        if !cache_dir.exists() {
+            return Ok(false);
+        }
+        let Some(file) = File::open(cache_dir.join("outputs.tar.zst")).ok() else {
+            return Ok(false);
+        };
+
+        let Some(decoder) = zstd::Decoder::new(file).ok() else {
+            return Ok(false);
+        };
+        let mut archive = tar::Archive::new(decoder);
+        if archive.unpack(".").is_err() {
+            return Ok(false);
+        }
         Ok(true)
     }
     fn save(
@@ -21,33 +37,50 @@ impl CacheProvider for LocalCacheProvider {
         metadata: CacheMetadata,
     ) -> anyhow::Result<bool> {
         let fyrer_dir = Path::new(DEFAULT_FYRER_DIR);
-        if !fyrer_dir.exists() {
-            std::fs::create_dir_all(fyrer_dir)?;
-        }
-        let cache_dir = fyrer_dir.join("cache").join(key);
+        std::fs::create_dir_all(fyrer_dir)?;
+
+        let cache_root = fyrer_dir.join("cache");
+        std::fs::create_dir_all(&cache_root)?;
+
+        let cache_dir = cache_root.join(key);
+
         if cache_dir.exists() {
             return Err(anyhow!("Cache directory already exists: {:?}", cache_dir));
         }
-        std::fs::create_dir_all(&cache_dir)?;
+
+        // create a temporary directory to write the cache files before renaming it to the actual cache directory. this ensures atomicity
+        let temp_dir = tempfile::Builder::new()
+            .prefix(&format!(".{key}."))
+            .tempdir_in(&cache_root)?;
+
         std::fs::write(
-            cache_dir.join("meta.json"),
+            temp_dir.path().join("meta.json"),
             serde_json::to_string(&metadata)?,
         )?;
 
-        let file = File::create(cache_dir.join("outputs.tar.zst"))?;
+        let file = File::create(temp_dir.path().join("outputs.tar.zst"))?;
         let encoder = zstd::Encoder::new(file, 1)?;
         let mut tar = tar::Builder::new(encoder);
+
         for output in source {
             if output.exists() {
                 tar.append_dir_all(output, output)?;
             }
         }
+
         let encoder = tar.into_inner()?;
         encoder.finish()?;
+
+        // publish the cache by renaming the temp file to actual file
+        std::fs::rename(temp_dir.path(), &cache_dir)?;
+
+        // prevent tempfile from trying to remove the directory after rename.
+        temp_dir.keep();
+
         Ok(true)
     }
     fn contains(&self, key: &str) -> bool {
-        let fyrer_dir = Path::new(".fyrer");
+        let fyrer_dir = Path::new(DEFAULT_FYRER_DIR);
         let cache_dir = fyrer_dir.join("cache").join(key).is_dir();
         cache_dir
     }
--- src/main.rs
+++ src/main.rs
@@ -26,21 +26,22 @@ async fn main() -> Result<()> {
     //
 
     let local_cache = LocalCacheProvider::default();
-    local_cache.save(
-        "2398nasdfklj34kj23lkj4",
-        &vec![PathBuf::from_str("examples/bun-http/dist").unwrap()],
-        CacheMetadata {
-            task: "api:build".to_string(),
-            hash: "2398nasdfklj34kj23lkj4".to_string(),
-            cmd: "bun build".to_string(),
-            dependencies: vec!["api:install".to_string()],
-            duration_ms: 1234,
-            exit_code: 0,
-            outputs: vec!["examples/bun-http/dist".to_string()],
-            cache: fyrer::cache::CacheStatus::Hit,
-            cache_key: Some("2398nasdfklj34kj23lkj4".to_string()),
-            timestamp: 1234567890,
-        },
-    )?;
+    // local_cache.save(
+    //     "2398nasdfklj34kj23lkj4",
+    //     &vec![PathBuf::from_str("examples/bun-http/dist").unwrap()],
+    //     CacheMetadata {
+    //         task: "api:build".to_string(),
+    //         hash: "2398nasdfklj34kj23lkj4".to_string(),
+    //         cmd: "bun build".to_string(),
+    //         dependencies: vec!["api:install".to_string()],
+    //         duration_ms: 1234,
+    //         exit_code: 0,
+    //         outputs: vec!["examples/bun-http/dist".to_string()],
+    //         cache: fyrer::cache::CacheStatus::Hit,
+    //         cache_key: Some("2398nasdfklj34kj23lkj4".to_string()),
+    //         timestamp: 1234567890,
+    //     },
+    // )?;
+    local_cache.restore("2398nasdfklj34kj23lkj4")?;
     Ok(())
 }
